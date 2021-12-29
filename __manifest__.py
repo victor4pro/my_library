15c00880fb25bc8b714b426a6da35047c231aeb9
victor4pro <victor4pro@gmail.com>
2021-12-28 22:01:40 -0300

    [ADD] my_library

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "My library",
+    'summary': "Manage books easily",
+    'description': """
+            Manage Library
+            ==============
+        Description related to library.
+    """,
+    'author': "Victor Landaeta",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '14.0.1',
+    'depends': ['base'],
+    'data': ['views/views.xml'],
+    'demo': ['demo.xml'],
+}
