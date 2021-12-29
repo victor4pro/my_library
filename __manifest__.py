ecac3592e9127f5744196fbbc5c7992534989254
victor4pro <victor4pro@gmail.com>
2021-12-28 22:01:40 -0300

    [ADD] my_library

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "My library",
+    'summary': "Manage books easily",
+    'author': "Victor Landaeta",
+    'website': "http://www.example.com",
+    'category': 'Sales',
+    'version': '14.0.1',
+    'depends': ['base'],
+    #'data': ['views/views.xml'],
+    #'demo': ['demo.xml'],
+}
